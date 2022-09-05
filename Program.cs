dcba0a03ed66d322eda35c9b25b334cfc71d8d99
Murillo Souza <murillo.souza@evolucao-itanhaem.com>
2022-09-04 23:49:26 -0300

    cores no refrão

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
-﻿Console.WriteLine("Eu não sei de onde vem");
-Console.WriteLine("Essa força que me leva pra você");
-Console.WriteLine("(Eu só sei que faz bem)");
-Console.WriteLine("(Mas confesso que no fundo eu duvidei)");
-Console.WriteLine("Tive medo e em segredo");
-Console.WriteLine("Guardei o sentimento e me sufoquei");
-Console.WriteLine("Mas agora é a hora");
-Console.WriteLine("Vou gritar pra todo mundo (joga a mão e grita)");
-Console.WriteLine("Eu tô apaixonado (eu tô contando tudo)");
-Console.WriteLine("(E não tô nem ligando pro que vão dizer)");
-Console.WriteLine("(Amar não é pecado e se eu tiver errado)");
+﻿                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Amar Não é Pecado - Luan Santana\n");              
+                Console.ResetColor();
+                
+                Console.WriteLine("Eu não sei de onde vem");
+                Console.WriteLine("Essa força que me leva pra você");
+                Console.WriteLine("(Eu só sei que faz bem)");
+                Console.WriteLine("Mas confesso que no fundo eu duvidei\n");
+              
+
+                Console.WriteLine("Tive medo e em segredo");
+                Console.WriteLine("Guardei o sentimento e me sufoquei");
+                Console.WriteLine("Mas agora é a hora");
+                Console.WriteLine("Vou gritar pra todo mundo\n");
+               
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Eu tô apaixonado eu tô contando tudo");
+                Console.WriteLine("(E não tô nem ligando pro que vão dizer)");
+                Console.WriteLine("Amar não é pecado e se eu tiver errado");
+                Console.WriteLine("Que se dane o mundo, eu só quero você");
+                Console.ResetColor();
+
